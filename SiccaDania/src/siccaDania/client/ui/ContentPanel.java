1d73b6ddc7907f583910ccbea5759bcf644979db
Danny-Lau <31654961+Danny-Lau@users.noreply.github.com>
2018-09-04 21:45:11 +0200

    First Commit

---
+++ SiccaDania/src/siccaDania/client/ui/ContentPanel.java
@@ -0,0 +1,13 @@
+package siccaDania.client.ui;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DeckLayoutPanel;
+
+public class ContentPanel extends Composite{
+	
+	 /**
+     * Hovedpanlet i klassen
+     */
+    private DeckLayoutPanel contentPanel;
+
+}
