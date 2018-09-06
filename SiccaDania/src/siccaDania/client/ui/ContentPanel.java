8a1932bccd076c42a038407307dc2e4209928d0e
Danny-Lau <31654961+Danny-Lau@users.noreply.github.com>
2018-09-06 10:29:43 +0200

    Begyndelse på projektet

--- SiccaDania/src/siccaDania/client/ui/ContentPanel.java
+++ SiccaDania/src/siccaDania/client/ui/ContentPanel.java
@@ -2,12 +2,43 @@ package siccaDania.client.ui;
 
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DeckLayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class ContentPanel extends Composite{
 	
 	 /**
-     * Hovedpanlet i klassen
+     * Hovedpanelet i klassen
      */
     private DeckLayoutPanel contentPanel;
+    
+    private StartScreen startScreen;
 
+    
+    public ContentPanel() {
+    	contentPanel = new DeckLayoutPanel();
+    	
+    	startScreen = new StartScreen();
+    	contentPanel.add(startScreen);
+    	
+    	
+    	
+    	
+    	//Det panel som vises npr programmet startes
+    	contentPanel.showWidget(startScreen);
+    	
+    	initWidget(contentPanel);
+    }
+    
+    //Denne metode benyttes til at ender View'et
+    public void changeView(Widget panel) {
+    	contentPanel.showWidget(panel);
+    }
+    
+    //Getter der henter de forskelige hoved-paneller.
+    
+    public StartScreen getStartScreen() {
+    	return startScreen;
+    }
+    
+  
 }
---
+++ SiccaDania/src/siccaDania/client/ui/StartScreen.java
@@ -0,0 +1,20 @@
+package siccaDania.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.UIObject;
+
+public class StartScreen extends UIObject {
+
+	private static StartScreeUiBinder uiBinder = GWT.create(StartScreeUiBinder.class);
+
+	interface StartScreeUiBinder extends UiBinder<Element, StartScreen> {
+	}
+
+	
+
+}
